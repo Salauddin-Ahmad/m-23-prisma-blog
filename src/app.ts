557b8e39bd7e90f33ad6d5fee165bc313f380dc2
Salauddin-Ahmad <salauddinahmed555@gmail.com>
2025-12-29 01:02:24 +0600

    post route and controller made

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Application } from 'express'
+import { postRouter } from './modules/post/post.router';
 
 const app: Application = express();
 
-app.use
+app.use("/posts", postRouter);
+
 app.get("/", (req, res) => {
     res.send("Hello, world");
 })
